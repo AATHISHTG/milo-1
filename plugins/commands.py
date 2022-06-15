3a55cce47e36c19f4e40f97081f56ca45f4636ea
AATHISHTG <107496049+AATHISHTG@users.noreply.github.com>
2022-06-15 07:18:12 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -37,7 +37,9 @@ async def start(client, message):
             )
         )
        
-        await asyncio.sleep(2) # 😢 https://github.com/EvamariaTG/EvaMaria/blob/master/plugins/p_ttishow.py#L17 😬 wait a bit, before checking.
+        await asyncio.sleep(30)
+        await fmsg.delete()
+                   # 😢 https://github.com/EvamariaTG/EvaMaria/blob/master/plugins/p_ttishow.py#L17 😬 wait a bit, before checking.
         if not await db.get_chat(message.chat.id):
             total=await client.get_chat_members_count(message.chat.id)
             await client.send_message(LOG_CHANNEL, script.LOG_TEXT_G.format(message.chat.title, message.chat.id, total, "Unknown"))       
@@ -251,9 +253,7 @@ async def start(client, message):
         caption=f_caption,
         protect_content=True if pre == 'filep' else False,
         )
-
-    await asyncio.sleep(30)
-    await fmsg.delete()                    
+                    
 
 @Client.on_message(filters.command('channel') & filters.user(ADMINS))
 async def channel_info(bot, message):
